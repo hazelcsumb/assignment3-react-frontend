4adc0b15121e870704fa3450c18b203db0eb89c1
hazelcsumb <bhazelton@csumb.edu>
2024-03-19 23:30:15 -0700

    Add error states, user feedback when there is no schedule

--- src/components/student/ScheduleView.js
+++ src/components/student/ScheduleView.js
@@ -11,7 +11,8 @@ import { SERVER_URL, semesters, years } from "../../Constants";
 
 const ScheduleView = (props) => {
   // schedule_view.js
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
+  const [showSchedule, setShowSchedule] = useState(false);
   const [schedule, setSchedule] = useState([]);
   const getClassSchedule = async (e) => {
     // Fetch schedule for the student from the server
@@ -20,31 +21,35 @@ const ScheduleView = (props) => {
     fetch(`${SERVER_URL}/enrollments?studentId=3&year=${year.value}&semester=${semester.value}`)
       .then((response) => response.json())
       .then((schedule) => {
+        setShowSchedule(true);
         setSchedule(schedule);
+        setError("");
       })
       .catch((error) => {
         console.error("Error fetching student schedule:", error);
+        setShowSchedule(false);
+        setError(error.message);
       });
   }
 
   const deleteCourse = async (enrollmentId) => {
-    const options =  {
+    const options = {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json', }
-    }; 
+    };
     fetch(`http://localhost:8080/enrollments/${enrollmentId}`, options)
-    .then((response) => response.json())
-    .then(({status, message}) => {
+      .then((response) => response.json())
+      .then(({ status, message }) => {
         if (status === 404) {
           throw new Error(message);
-        } 
+        }
         setSchedule(schedule.filter((course) => course.enrollmentId !== enrollmentId));
         setError("");
-    })
-    .catch((error) => {
-      console.error("Error deleting course", error);
-      setError(error.message);
-    })
+      })
+      .catch((error) => {
+        console.error("Error deleting course", error);
+        setError(error.message);
+      })
   }
 
   return (
@@ -67,15 +72,23 @@ const ScheduleView = (props) => {
         <button type="submit">View Class Schedule</button>
       </form>
       <div id="schedule">
+
         {
-          schedule.map((course) => (
-              <div key={course.courseId}>
-                {`Course: ${course.courseId}, Time: ${course.times}`}
-                <button type="submit" onClick={() => deleteCourse(course.enrollmentId)}>Drop course</button>
-              </div>
-          ))
+          schedule.length === 0 && showSchedule ?
+            (
+              <p style={{ color: "red", margin: 0 }}>No Schedule found!</p>
+            )
+            :
+            (
+              schedule.map((course) => (
+                <div key={course.courseId}>
+                  {`Course: ${course.courseId}, Time: ${course.times}`}
+                  <button type="submit" onClick={() => deleteCourse(course.enrollmentId)}>Drop course</button>
+                </div>
+              ))
+            )
         }
-        <p style={{color:"red", margin:0}}>{error}</p>
+        <p style={{ color: "red", margin: 0 }}>{error}</p>
       </div>
     </div>
   );
