378e9c81ec26d790f6fafd33421e103e298f50d3
YRedmon <yredmon@csumb.edu>
2024-03-19 21:20:13 -0700

    Assgn Update & View

--- src/components/instructor/AssignmentUpdate.js
+++ src/components/instructor/AssignmentUpdate.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 
 //  instructor updates assignment title, dueDate 
 //  use an mui Dialog
 //  issue PUT to URL  /assignments with updated assignment
 
 const AssignmentUpdate = (props)  => {
+
+    const [open, setOpen] = useState(false);
+    const [editMessage, setEditMessage] = useState('');
+    const [assignment, setAssignment] = useState({assignmentId:'', title:'', dueDate:''});
+
+    /*
+     *  dialog for edit course
+     */
+    const editOpen = (event) => {
+        setOpen(true);
+        setEditMessage('');
+        setAssignment(props.assignment);
+    };
+
+    const editClose = () => {
+        setOpen(false);
+        setAssignment({assignmentId:'', title:'', dueDate:''});
+
+    };
+
+    const editChange = (event) => {
+        setAssignment({...assignment,  [event.target.name]:event.target.value})
+    }
+
+    const onSave = () => {
+        if (assignment.assignmentId==='') {
+            setEditMessage("AssignmentId can not be blank");
+        } else if (assignment.title==='') {
+            setEditMessage("Title can not be blank");
+        } else {
+            props.save(assignment);
+            editClose();
+        }
+    }
 
     // assignment_update.js
     document.getElementById('updateForm').addEventListener('submit', function(event) {
@@ -37,7 +72,20 @@ const AssignmentUpdate = (props)  => {
 
     return (
         <>
-          <h3>Not implemented</h3>  
+            <Button onClick={editOpen}>Edit</Button>
+            <Dialog open={open} >
+                <DialogTitle>Edit Assignment</DialogTitle>
+                <DialogContent  style={{paddingTop: 20}} >
+                    <h4>{editMessage}</h4>
+                    <TextField style={{padding:10}} autoFocus fullWidth label="assignmentId" name="assignmentId" value={assignment.assignmentId}  InputProps={{readOnly: true, }}  />
+                    <TextField style={{padding:10}} fullWidth label="title" name="title" value={assignment.title} onChange={editChange}  />
+                    <TextField style={{padding:10}} fullWidth label="dueDate" name="dueDate" value={assignment.dueDate} onChange={editChange}  />
+                </DialogContent>
+                <DialogActions>
+                    <Button color="secondary" onClick={editClose}>Close</Button>
+                    <Button color="primary" onClick={onSave}>Save</Button>
+                </DialogActions>
+            </Dialog>
         </>                       
     )
 }
