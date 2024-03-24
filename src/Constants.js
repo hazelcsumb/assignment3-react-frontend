fd6b16b002be27c817f91b0d97f0f7a714e195f5
hazelcsumb <bhazelton@csumb.edu>
2024-03-23 20:04:26 -0700

    Add studentId to constants, will change later

--- src/Constants.js
+++ src/Constants.js
@@ -2,3 +2,4 @@ export const SERVER_URL = 'http://localhost:8080';
 export const userTypes = ['STUDENT', 'ADMIN', 'INSTRUCTOR'];
 export const semesters = ['Spring', 'Fall'];
 export const years = [2022,2023,2024];
+export const studentId = 3;
