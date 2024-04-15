1ee1f1ae3ca91ba5f383d7c2a069adc572214df3
hazelcsumb <bhazelton@csumb.edu>
2024-04-15 00:56:31 -0700

    Add documentation for Axios API feature

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,28 @@
 import axios from "axios";
 import { baseURL } from "../Constants";
 
+/*
+ *
+ *
+ * GET call
+ * let response = await api.get(`${baseURL}/some/route`);
+ *
+ * POST call
+ * response = await api.post(`${baseURL}/some/route`, body);
+ *
+ * The data is stored in response.data
+ * setData(response.data);
+ *
+ * DELETE call
+ * api.delete("/some/route");
+ *
+ * PUT call
+ * api.put("/some/route");
+ *
+ * View sample code in src/student/ScheduleView.js to see how it is used
+ * with try/catch blocks and async/await
+ *
+*/
 export const api = axios.create({
   baseURL: baseURL,
   headers: {
