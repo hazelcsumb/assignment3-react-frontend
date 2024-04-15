dc5bba76244d304f747f7f5b6b1bae342d50b8b8
hazelcsumb <bhazelton@csumb.edu>
2024-04-15 00:10:13 -0700

    Implement api feature to prevent repetitive code while fetching

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios, { AxiosInstance } from "axios";
+import { baseURL } from "../Constants";
+
+export const api = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    'Authorization': sessionStorage.getItem("jwt"),
+  },
+});
