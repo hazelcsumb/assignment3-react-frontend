1c44fb1dbe60742b3e93ca9bd03579e75bfa6105
hazelcsumb <bhazelton@csumb.edu>
2024-04-15 00:50:16 -0700

    Use localhost:8080 as baseURL since we are no longer using micro-services

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,4 @@
-export const REGISTRAR_URL = 'http://localhost:8080';
-export const GRADEBOOK_URL = 'http://localhost:8080';
+export const baseURL = 'http://localhost:8080';
 
 export const userTypes = ['STUDENT', 'ADMIN', 'INSTRUCTOR'];
 export const years = [2022,2023,2024];
